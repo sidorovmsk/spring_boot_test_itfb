d3e31b0c5284184d6b1775694119e0b3e772ebc0
a.v.sidorov <a.v.sidorov@itfbgroup.ru>
2023-10-18 19:50:04 +0300

    Initial commit

---
+++ src/main/java/spring_boot_java/test_itfb/TestItfbApplication.java
@@ -0,0 +1,13 @@
+package spring_boot_java.test_itfb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestItfbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestItfbApplication.class, args);
+	}
+
+}
