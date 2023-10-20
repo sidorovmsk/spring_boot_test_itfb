e1ee573fba8669ac105005a9f2629b7a6e811033
a.v.sidorov <a.v.sidorov@itfbgroup.ru>
2023-10-20 22:47:44 +0300

    + modelMapper

--- src/main/java/spring_boot_java/test_itfb/TestItfbApplication.java
+++ src/main/java/spring_boot_java/test_itfb/TestItfbApplication.java
@@ -1,7 +1,9 @@
 package spring_boot_java.test_itfb;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TestItfbApplication {
@@ -10,4 +12,8 @@ public class TestItfbApplication {
 		SpringApplication.run(TestItfbApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 }
