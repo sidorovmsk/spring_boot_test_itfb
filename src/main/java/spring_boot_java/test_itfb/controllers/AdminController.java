9a4b0aebb8ddf50772c0e39fe9f79f0614ae714c
a.v.sidorov <a.v.sidorov@itfbgroup.ru>
2023-10-28 21:49:38 +0300

    refactor: AdminController

--- src/main/java/spring_boot_java/test_itfb/controllers/AdminController.java
+++ src/main/java/spring_boot_java/test_itfb/controllers/AdminController.java
@@ -1,36 +1,24 @@
 package spring_boot_java.test_itfb.controllers;
 
 import lombok.extern.slf4j.Slf4j;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import spring_boot_java.test_itfb.dto.PersonDto;
-import spring_boot_java.test_itfb.exceptions.PersonNotFoundException;
 import spring_boot_java.test_itfb.models.Person;
-import spring_boot_java.test_itfb.repositories.PeopleRepository;
 import spring_boot_java.test_itfb.services.AdminService;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Controller
 public class AdminController {
     private final AdminService adminService;
-    private final PeopleRepository peopleRepository;
-    private final ModelMapper modelMapper;
-    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public AdminController(AdminService adminService, PeopleRepository peopleRepository, ModelMapper modelMapper, PasswordEncoder passwordEncoder) {
+    public AdminController(AdminService adminService) {
         this.adminService = adminService;
-        this.peopleRepository = peopleRepository;
-        this.modelMapper = modelMapper;
-        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/users")
@@ -45,14 +33,7 @@ public class AdminController {
     public ResponseEntity<List<PersonDto>> getUsersJsonList() {
         log.info("GET request to /api/users");
         adminService.doAdminStuff();
-        List<PersonDto> peopleDto = peopleRepository.findAll().stream()
-                .map(user -> {
-                    PersonDto personDto = modelMapper.map(user, PersonDto.class);
-                    personDto.setRole(personDto.getRole().replace("ROLE_", ""));
-                    return personDto;
-                })
-                .collect(Collectors.toList());
-        return ResponseEntity.ok(peopleDto);
+        return adminService.getUsersJsonList();
     }
 
     @ResponseBody
@@ -60,13 +41,7 @@ public class AdminController {
     public ResponseEntity<?> getUserJsonById(@PathVariable("id") int id) {
         log.info("GET request to /user_json/" + id);
         adminService.doAdminStuff();
-        Optional<Person> person = peopleRepository.findById(id);
-        if (person.isEmpty()) {
-            throw new PersonNotFoundException("Пользователь с идентификатором " + id + " не найден.");
-        }
-        PersonDto personDto = modelMapper.map(person, PersonDto.class);
-        personDto.setRole(personDto.getRole().replace("ROLE_", ""));
-        return ResponseEntity.ok(personDto);
+        return adminService.getUserJsonById(id);
     }
 
     @GetMapping("/user/{id}")
@@ -81,13 +56,7 @@ public class AdminController {
     public ResponseEntity<?> deleteUserById(@PathVariable("id") int id) {
         log.info("DELETE request to /user_delete/" + id);
         adminService.doAdminStuff();
-        System.out.println("deleting");
-        if (peopleRepository.existsById(id)) {
-            peopleRepository.deleteById(id);
-            return ResponseEntity.ok("User with ID " + id + " has been deleted.");
-        } else {
-            throw new PersonNotFoundException("Пользователь с идентификатором " + id + " не найден.");
-        }
+        return adminService.deleteUserById(id);
     }
 
     @GetMapping("/user_edit/{id}")
@@ -102,21 +71,8 @@ public class AdminController {
     public ResponseEntity<?> editUserById(@PathVariable("id") int id,
                                           @RequestBody Person updatedPerson) {
         log.info("POST request to /user_edit/" + id);
-        System.out.println("saving");
         adminService.doAdminStuff();
-        Person person = peopleRepository.findById(id).orElse(null);
-        if (person == null) {
-            throw new PersonNotFoundException("Пользователь с идентификатором " + id + " не найден.");
-        } else {
-            person.setUsername(updatedPerson.getUsername());
-            person.setRole(updatedPerson.getRole());
-            if (!updatedPerson.getPassword().equals("")) {
-                person.setPassword(passwordEncoder.encode(updatedPerson.getPassword()));
-            }
-            person.setEnabled(updatedPerson.isEnabled());
-            peopleRepository.save(person);
-            return ResponseEntity.ok("User with ID " + id + " has been deleted.");
-        }
+        return adminService.editUserById(id, updatedPerson);
     }
 }
 
--- src/main/java/spring_boot_java/test_itfb/services/AdminService.java
+++ src/main/java/spring_boot_java/test_itfb/services/AdminService.java
@@ -1,13 +1,81 @@
 package spring_boot_java.test_itfb.services;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import spring_boot_java.test_itfb.dto.PersonDto;
+import spring_boot_java.test_itfb.exceptions.PersonNotFoundException;
+import spring_boot_java.test_itfb.models.Person;
+import spring_boot_java.test_itfb.repositories.PeopleRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class AdminService {
+    private final PeopleRepository peopleRepository;
+    private final ModelMapper modelMapper;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public AdminService(PeopleRepository peopleRepository, ModelMapper modelMapper, PasswordEncoder passwordEncoder) {
+        this.peopleRepository = peopleRepository;
+        this.modelMapper = modelMapper;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_SOME_OTHER')")
     public void doAdminStuff() {
         System.out.println("Only admin here");
+    }
+
+    public ResponseEntity<List<PersonDto>> getUsersJsonList() {
+        List<PersonDto> peopleDto = peopleRepository.findAll().stream()
+                .map(user -> {
+                    PersonDto personDto = modelMapper.map(user, PersonDto.class);
+                    personDto.setRole(personDto.getRole().replace("ROLE_", ""));
+                    return personDto;
+                })
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(peopleDto);
+    }
+
+    public ResponseEntity<?> getUserJsonById(int id) {
+        Optional<Person> person = peopleRepository.findById(id);
+        if (person.isEmpty()) {
+            throw new PersonNotFoundException("Пользователь с идентификатором " + id + " не найден.");
+        }
+        PersonDto personDto = modelMapper.map(person, PersonDto.class);
+        personDto.setRole(personDto.getRole().replace("ROLE_", ""));
+        return ResponseEntity.ok(personDto);
+    }
+
+    public ResponseEntity<?> deleteUserById(int id) {
+        if (peopleRepository.existsById(id)) {
+            peopleRepository.deleteById(id);
+            return ResponseEntity.ok("User with ID " + id + " has been deleted.");
+        } else {
+            throw new PersonNotFoundException("Пользователь с идентификатором " + id + " не найден.");
+        }
+    }
+
+    public ResponseEntity<?> editUserById(int id, Person updatedPerson) {
+        Person person = peopleRepository.findById(id).orElse(null);
+        if (person == null) {
+            throw new PersonNotFoundException("Пользователь с идентификатором " + id + " не найден.");
+        } else {
+            person.setUsername(updatedPerson.getUsername());
+            person.setRole(updatedPerson.getRole());
+            if (!updatedPerson.getPassword().equals("")) {
+                person.setPassword(passwordEncoder.encode(updatedPerson.getPassword()));
+            }
+            person.setEnabled(updatedPerson.isEnabled());
+            peopleRepository.save(person);
+            return ResponseEntity.ok("User with ID " + id + " has been deleted.");
+        }
     }
 }
